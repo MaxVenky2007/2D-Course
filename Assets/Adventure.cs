4712d710575a73638a3dc4acab4a96c079b04d73
Seshan <87255359+MaxVenky2007@users.noreply.github.com>
2021-07-11 14:37:19 +0530

    Array challenge

--- Assets/Adventure.cs
+++ Assets/Adventure.cs
@@ -8,12 +8,15 @@ public class Adventure : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
+    string[] weekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
     State state;
     // Start is called before the first frame update
     void Start()
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+        Debug.Log(weekDays[2]);
     }
 
     // Update is called once per frame
